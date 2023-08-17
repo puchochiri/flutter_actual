a8d630d563a7f2491d688ae5357e504f1cf6d066
puchochiri <doawishfor@naver.com>
2023-08-17 20:03:00 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_actual/common//component/custom_text_form_field.dart';
+import 'package:flutter_actual/user/view/login_screen.dart';
+
+void main() {
+  runApp(
+    _App(),
+  );
+}
+
+class _App extends StatelessWidget {
+  const _App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+
+    );
+  }
+}
+
+
